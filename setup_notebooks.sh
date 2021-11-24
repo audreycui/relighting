6a085a7b3fb08af48cb671a1c8146299d5154500
Ali Jahanian <ali-design@csail.mit.edu>
2021-11-24 09:22:16 -0500

    clean

--- setup_notebooks.sh
+++ setup_notebooks.sh
@@ -8,14 +8,3 @@ git config filter.clean_ipynb.clean "$(pwd)/ipynb_drop_output.py"
 git config filter.clean_ipynb.smudge cat
 git config filter.clean_ipynb.required true
 
-# Set up symlinks for the example notebooks
-# mkdir -p ../datasets
-# mkdir -p ../results
-# ln -sfn ../datasets -t .
-# ln -sfn ../results -t .
-# ln -sfn ../models -t .
-# ln -sfn ../utils -t .
-# ln -sfn ../torch_utils -t .
-# ln -sfn ../dnnlib -t .
-# ln -sfn ../experiment -t .
-# ln -sfn ../evaldata -t .
